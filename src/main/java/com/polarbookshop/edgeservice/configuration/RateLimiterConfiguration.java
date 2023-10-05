8bf2aef38c41ac8545bde794ad4e3629f295324b
burulko <alesanula0821@gmail.com>
2023-10-05 18:27:21 +0300

    edge-service initial commit

---
+++ src/main/java/com/polarbookshop/edgeservice/configuration/RateLimiterConfiguration.java
@@ -0,0 +1,15 @@
+package com.polarbookshop.edgeservice.configuration;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfiguration {
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just("anonymous");
+    }
+}
